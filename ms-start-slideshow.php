16ec0069f9c0310eaa0be5635c9ae317121e61d7
Philip Rudy <me@philiparudy.com>
2024-08-24 19:50:55 -0500

    Add rss namespace/update documentation

--- ms-start-slideshow.php
+++ ms-start-slideshow.php
@@ -31,16 +31,21 @@ if(!defined('MSSS_ASSETS_VERSION')) {
     define('MSSS_ASSETS_VERSION', '1.3.12');
 }
 
-// We want to get the meta slider post
-// if there is a category of slideshow on the wordpress post we want to get the slideshow and display it, as well as make
-// the ress feed have a custom entry for the slideshow
+/**
+ * Functionality to add slideshow xml to any post that has non-empty msss_slides fields from ACF. This code should remain at the bottom
+ * of the functions.php file. For additional customizations, a small plugin can be built.
+ * When originally built, the ACF fields were set to only display on posts with a category of 'slideshows'
+ * Contact me@philiparudy.com for troubleshooting with subject "Microsoft Start Plugin Help."
+ * 
+ * 
+ * */
 class MsStartSlideshow {
     public function __construct() {
         // If ACF plugin is installed, handle the ACF slides
         if(class_exists('ACF')) {
-            add_action('the_content', [$this, 'maybe_do_acf_slides']);
+            add_action('the_content', [$this, 'maybe_do_acf_slides'], 1);
             add_action('rss2_item', [$this, 'maybe_add_slideshow_to_rss_feed']);
-            add_action('rss2_ns', 'add_custom_rss_namespaces');
+        	add_action('rss2_ns', [$this, 'add_custom_rss_namespaces']);
         }
     }
 
@@ -51,7 +56,6 @@ class MsStartSlideshow {
     // Add slides to the RSS feed in the correct format
     public function maybe_add_slideshow_to_rss_feed() {
         if (have_rows('msss_slides')) {
-            echo '<media:group>';
             while (have_rows('msss_slides')) {
                 the_row();
 
@@ -74,45 +78,39 @@ class MsStartSlideshow {
                     echo '</media:content>';
                 }
             }
-            echo '</media:group>';
         }
     }
 
     // Add slideshow to the content output
     public function maybe_do_acf_slides($content) {
-        $post_id = get_the_ID();
-        $category = get_the_category($post_id);
-        $html = '';
+		$html = '';
+		
+		if (have_rows('msss_slides')) {
+			$html = '<div class="slideshow" style="padding: 15px 0;">';
 
-        if ($category) {
-            foreach ($category as $cat) {
-                if ('slideshows' === $cat->name || 'slideshow' === $cat->name) {
-                    if (have_rows('msss_slides')) {
-                        $html = '<div class="slideshow" style="padding: 20px 0;">';
+			// Optional: Author and Title elements for slideshow inline
+			
 
-                        // Optional: Author and Title elements for slideshow inline
-                        $html .= '<cite>' . get_the_author() . '</cite>';
-                        $html .= '<title>' . get_the_title() . '</title>';
+			while (have_rows('msss_slides')) {
+				the_row();
+				
+				$html .= '<div class="slideshow_slide" style="padding: 10px 0;">';
+				$html .= '<div class="slideshow_slide_title"><h2>' . get_sub_field('msss_slide_title') . '</h2></div>';
 
-                        while (have_rows('msss_slides')) {
-                            the_row();
+				$html .= '<figure>';
+				$html .= '<img src="' . get_sub_field('msss_slide_image')['url'] . '" alt="' . get_sub_field('msss_slide_title') . '">';
+				$html .= '<figcaption>' . get_sub_field('msss_slide_description');
 
-                            $html .= '<figure>';
-                            $html .= '<img src="' . get_sub_field('msss_slide_image')['url'] . '" alt="' . get_sub_field('msss_slide_title') . '">';
-                            $html .= '<figcaption>' . get_sub_field('msss_slide_description');
+				if(get_sub_field('msss_slide_image_credit')) {
+					$html .= '<span class="copyright">' . get_sub_field('msss_slide_image_credit') . '</span>'; // Assuming alt text for copyright
+				}
 
-                            if(get_sub_field('msss_slide_image_credit')) {
-                                $html .= '<span class="copyright">' . get_sub_field('msss_slide_image_credit') . '</span>'; // Assuming alt text for copyright
-                            }
-                            
-                            $html .= '</figcaption>';
-                            $html .= '</figure>';
-                        }
-                        $html .= '</div>';
-                    }
-                }
-            }
-        }
+				$html .= '</figcaption>';
+				$html .= '</figure>';
+				$html .= '</div>';
+			}
+			$html .= '</div>';
+		}
 
         return $content . $html;
     }
@@ -121,5 +119,3 @@ class MsStartSlideshow {
 add_action('wp', function() {
     $msStartSlideshow = new MsStartSlideshow();
 });
-
-
